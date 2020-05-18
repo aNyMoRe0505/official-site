8d688cfbe1bfc671ec70b7f542f5d11799df829f
Paul Wang 王晨宇 <paul.wang@TWTPMAC10004197.local>
2020-05-18 16:41:01 +0800

    update List style

--- src/components/blog/List.jsx
+++ src/components/blog/List.jsx
@@ -5,12 +5,25 @@ import PropTypes from 'prop-types';
 import SubTitle from './SubTitle';
 import Text from './Text';
 
+const Wrapper = styled.div`
+  width: 100%;
+  margin: 30px 0 0;
+  @media (max-width: 768px) {
+    margin: 15px 0 0;
+  };
+  ${({ titleExist }) => !titleExist && 'margin: 10px 0px 0px'};
+`;
+
 const TextWrap = styled.div`
   display: flex;
   align-items: flex-start;
   justify-content: center;
   margin: 10px 0 0;
   width: 100%;
+`;
+
+const Title = styled(SubTitle)`
+  margin: 0;
 `;
 
 const NumberDot = styled(Text)`
@@ -29,11 +42,11 @@ function List({
   prefixType,
 }) {
   return (
-    <>
+    <Wrapper titleExist={!!title}>
       {title && (
-      <SubTitle strong>
+      <Title strong>
         {title}
-      </SubTitle>
+      </Title>
       )}
       {list.map((i, index) => (
         <TextWrap key={i.text}>
@@ -45,7 +58,7 @@ function List({
           </SmallMarginText>
         </TextWrap>
       ))}
-    </>
+    </Wrapper>
   );
 }
 
