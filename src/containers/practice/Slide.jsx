16ae922455650740a8707da6e5d341f4b17e653a
Paul Wang 王晨宇 <paul.wang@TWTPMAC10004197.local>
2020-05-19 13:56:43 +0800

    chore layout

--- src/containers/practice/Slide.jsx
+++ src/containers/practice/Slide.jsx
@@ -137,13 +137,13 @@ function Slide() {
             {imagesArray.map((image, index) => (
               <Image
                 customStyle={css`
-              @media (max-width: 768px) {
-                width: ${`${slideWidth / 2}px`};
-              };
-              @media (max-width: 414px) {
-                width: ${`${slideWidth}px`};
-              };
-            `}
+                  @media (max-width: 768px) {
+                    width: ${`${slideWidth / 2}px`};
+                  };
+                  @media (max-width: 414px) {
+                    width: ${`${slideWidth}px`};
+                  };
+                `}
                 translateXValue={(index - currentIndex) * ((slideWidth / slideToShow) + 5)}
                 key={`${image}-${index}`}
                 src={image}
