7b75c1cb2b971bde0406fb7c530ceab842de3fcd
Paul Wang 王晨宇 <paul.wang@TWTPMAC10004197.local>
2020-06-23 15:55:08 +0800

    update dragBox logic

--- src/components/practice/rxjs/DragBox.jsx
+++ src/components/practice/rxjs/DragBox.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { fromEvent } from 'rxjs';
 import {
@@ -7,7 +7,7 @@ import {
   takeUntil,
   withLatestFrom,
   tap,
-  delay,
+  // delay,
 } from 'rxjs/operators';
 
 const Wrapper = styled.div`
@@ -30,17 +30,16 @@ const Box = styled.div`
   cursor: grab;
   top: 0;
   left: 0;
-  transform: translate(0px, 0px);
   z-index: ${({ zIndex }) => zIndex};
 `;
 
 const boxArray = ['red', 'orange', 'yellow', 'green', 'blue', 'black', 'gray'];
 
 function DragBox() {
-  const firstRef = useRef();
-
   useEffect(() => {
-    const { x: rectX, y: rectY } = firstRef.current.getBoundingClientRect();
+    let originalLeft = 0;
+    let originalTop = 0;
+
     const allDragBox = document.getElementsByClassName('dragBox');
 
     const mouseDown = fromEvent(allDragBox[0], 'mousedown');
@@ -50,6 +49,9 @@ function DragBox() {
     const mouseDownEvent = mouseDown.pipe(
       tap(() => {
         allDragBox[0].style.cursor = 'grabbing';
+        const numRegex = /\d+/g;
+        originalLeft = parseInt(window.getComputedStyle(allDragBox[0]).left.match(numRegex)[0], 10);
+        originalTop = parseInt(window.getComputedStyle(allDragBox[0]).top.match(numRegex)[0], 10);
       }),
       flatMap(() => mouseMove.pipe(
         takeUntil(mouseUp.pipe(
@@ -59,26 +61,37 @@ function DragBox() {
         )),
       )),
       withLatestFrom(mouseDown, (moveEvent, downEvent) => ({
-        x: moveEvent.clientX - downEvent.offsetX - rectX,
-        y: moveEvent.clientY - downEvent.offsetY - rectY,
+        x: moveEvent.clientX - downEvent.clientX + originalLeft,
+        y: moveEvent.clientY - downEvent.clientY + originalTop,
       })),
     );
 
-    const subscribedEvent = [];
+    // const subscribedEvent = [];
 
-    Array.from(allDragBox).forEach((element, index) => {
-      const event = mouseDownEvent.pipe(
-        delay(0 + 100 * index),
-      ).subscribe(({ x, y }) => {
-        element.style.transform = `translate(${x}px, ${y}px)`;
+    // Array.from(allDragBox).forEach((element, index) => {
+    //   const event = mouseDownEvent.pipe(
+    //     delay(0 + 100 * index),
+    //   ).subscribe(({ x, y }) => {
+    //     element.style.top = `${y}px`;
+    //     element.style.left = `${x}px`;
+    //   });
+    //   subscribedEvent.push(event);
+    // });
+
+    const event = mouseDownEvent.subscribe(({ x, y }) => {
+      Array.from(allDragBox).forEach((element, index) => {
+        setTimeout(() => {
+          element.style.top = `${y}px`;
+          element.style.left = `${x}px`;
+        }, 0 + 100 * index);
       });
-      subscribedEvent.push(event);
     });
 
     return () => {
-      subscribedEvent.forEach((event) => {
-        event.unsubscribe();
-      });
+      event.unsubscribe();
+      // subscribedEvent.forEach((event) => {
+      //   event.unsubscribe();
+      // });
     };
   }, []);
 
@@ -87,7 +100,6 @@ function DragBox() {
       {boxArray.map((color, index) => (
         <Box
           color={color}
-          ref={(index === 0 && firstRef) || null}
           className="dragBox"
           zIndex={boxArray.length - index}
           key={`dragBox-${index + 1}`}
