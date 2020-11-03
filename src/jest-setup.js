f5db7d1a7b12c41e002d038fdbd794d9274e0474
Paul Wang <anymore0505@gmail.com>
2020-11-03 17:22:45 +0800

    update mockIntersectionObserver

--- src/jest-setup.js
+++ src/jest-setup.js
@@ -3,4 +3,12 @@ import '@testing-library/jest-dom';
 
 const noop = () => {};
 Object.defineProperty(window, 'scrollTo', { value: noop, writable: true });
-Object.defineProperty(window, 'IntersectionObserver', { value: noop, writable: true });
+
+class MockIntersectionObserver {
+  constructor() {
+    this.observe = noop;
+    this.unobserve = noop;
+  }
+}
+
+Object.defineProperty(window, 'IntersectionObserver', { value: MockIntersectionObserver, writable: true });
